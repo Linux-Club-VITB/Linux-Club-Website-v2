dbba992133f1b1eb3fc8e1f0f1b3878ee19e7cfe
Romit  Dey <romit77dey@gmail.com>
2024-04-21 00:17:50 +0530

    hero section changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Navbar from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
+import Events from "@/components/Events";
+import Contact from "@/components/Contact";
+import Team from "@/components/Team";
 
 export default function page() {
   return (
     <div>
+      <Navbar />
       <Hero />
+      <Events />
+      <Contact />
+      <Team />
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import * as React from "react";
 import { Vortex } from "./ui/vortex";
+import { motion } from "framer-motion";
 
 export function Hero() {
   return (
@@ -10,19 +13,29 @@ export function Hero() {
         particleCount={500}
         className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
       >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+        <motion.h2
+          className="text-white text-2xl md:text-6xl font-bold text-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.85 }}
+        >
           Linux Club VIT Bhopal
-        </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+        </motion.h2>
+        <motion.p
+          className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+        >
           Welcome to the Linux Club Lorem ipsum dolor sit amet consectetur
           adipisicing elit. In, maiores.
-        </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+        </motion.p>
+        {/* <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
             Order now
           </button>
           <button className="px-4 py-2  text-white ">Watch trailer</button>
-        </div>
+        </div> */}
       </Vortex>
     </div>
   );
