1343b7bc12fc363efd71fedaf25f30ef5372a1b6
Romit  Dey <romit77dey@gmail.com>
2024-04-20 23:39:49 +0530

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Hero } from "@/components/Hero";
 
 export default function page() {
   return (
     <div>
-      <h1>Landing Page on test branch</h1>
+      <Hero />
     </div>
   );
 }
